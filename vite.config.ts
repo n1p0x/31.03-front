6118f98c300bb1bd17a3b01701b1367219c5cd78
Kasum <kasum2605@mail.ru>
2025-03-31 14:58:43 +0400

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react(), tsconfigPaths()],
+	base: '/',
+	plugins: [
+        tailwindcss(),
+		react(),
+		tsconfigPaths(),
+		nodePolyfills(),
+	],
 })
